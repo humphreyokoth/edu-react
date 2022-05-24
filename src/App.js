bb00faeec8a3d5b0bfe367d70c8bd462d7894020
humphreyokoth <humpasp@gmail.com>
2022-05-24 09:32:16 +0300

    home route

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@
 import './App.css';
 import {Routes,Route,BrowserRouter} from 'react-router-dom';
 import Login from "./pages/Login";
+import Home from './pages/Home';
 
 function App() {
   return (
     <BrowserRouter>
     <Routes>
       <Route path ="/login" element ={<Login/>}/>
+      <Route path ="/" element ={<Home/>}/>
     </Routes>
 
     </BrowserRouter>
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,9 +1,27 @@
-import React from "react";
+import React,{useEffect,useState} from "react";
+import { Navigate } from "react-router-dom";
 
 const Home =()=>{
+ const [user,setUser] = useState(null);
 
 
+  useEffect (()=>{
+    const user = JSON.parse(localStorage.getItem('uset'))
+    if(uset){
+        setUser(user);
+    }else{
+        Navigate("/login")
+    }
+  },[])
+    return(
+        <div className="App">
+    <h1>Home</h1>
+    <h3> Welcome{user.email}</h3>
 
-
+        
+        </div>
+    )
 
 }
+
+export default Home
