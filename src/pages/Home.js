ba6e44d10cb536e15bc2f0353acda098a140ac04
humphreyokoth <humpasp@gmail.com>
2022-05-24 15:09:17 +0300

    parcel api

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,16 +1,27 @@
 import React,{useEffect,useState} from "react";
 import { useNavigate } from "react-router-dom";
+import Api from "../api";
 
 const Home =()=>{
 const navigate = useNavigate();
  const [user,setUser] = useState(null);
+ const [loading, setLoading] = useState(true);
+ const [parcels,setParcels] = useState([]);
 
+     
+ const fetchParcels = async ()=>{
+     const res = await Api.get("/parcels");
+     const data = await res.json();
+     setParcels(data.parcels);
+     setLoading(false);
 
+ }
   useEffect (()=>{
     const _user = JSON.parse(localStorage.getItem('user'))
     if(typeof _user!== "undefined" || _user !== null){
         setUser(_user);
     }
+    fetchParcels();
     if(_user === null){
         navigate("/login")
     }
@@ -19,6 +30,18 @@ const navigate = useNavigate();
         <div className="App">
     <h1>Home</h1>
     <h3> Welcome{user?.email}</h3>
+        <h3>Parcels</h3>
+        {loading ?<h1>Fetching parcels ...</h1>:<ul>
+                {parcels.map(()=>{
+
+                    return <li key ={parcels.id}>
+                        <h4>{parcels.name}</h4>
+                        <p>{parcels.description}</p>
+
+                    </li>
+                })}
+
+        </ul>}
 
         
         </div>
